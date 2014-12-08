da6687cf2b1b168662cd776f217213e0e85fd0e7
hparsa741 <hparsa@gmail.com>
2014-12-07 20:07:03 -0500

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following code calculates the inverse of a given matrix as input argument ...
+## to makeCacheMatrix() function. Then cachesolve() can be used to either calculate ...
+## the inteverse of the matrix or return the cashed value of the inverse from memory
 
-## Write a short comment describing this function
+## makeCacheMatrix() can get a metrix as an input argument and assign ...
+## memory location to the inverse and the matrix itself.
+## memory location can be display by just caling the faunction without assignment.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmat <- function(solve) m <<- solve
+  getmat <- function() m
+  list(set = set, get = get,
+       setmat = setmat,
+       getmat = getmat)
 }
 
+## cachesolve() get the output of makeCacheMatrix() which are memory locations ...
+## with or without the values of inverse. If there is no inverse ...
+## calculated (i.e. the inverse is null and memory location is empty) it will ...
+## be calculated otherwise the cashed value from memory will be returen.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x, ...) {
+  
+  m <- x$getmat()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmat(m)
+  m  
 }
